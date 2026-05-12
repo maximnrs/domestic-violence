185c3de0ff12f331cbc54889e7715a3e7947718b
ThijsvdWeijer <thijsvdweijer@gmail.com>
2026-05-12 13:23:28 +0200

    First draft for config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,27 @@
-testing git link 
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+
+    #database
+    database_url: str
+
+    #MinIO (Truenas)
+    minio_endpoint: str
+    minio_access_key: str
+    minio_secret_key: str
+    minio_bucket: str
+
+    #OpenBao (Vault)
+    vault_url: str
+    vault_token: str
+
+    #JWT Auth
+    secret_key: str
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
+
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
