cd08515b7cc71112086672f49f28ef4a3fff98e6
maximnrs <maxim.nabers@gmail.com>
2026-06-01 14:16:21 +0200

    Create settings tab

--- mobile-app-frontend/domestic-violence-app/app/(tabs)/_layout.tsx
+++ mobile-app-frontend/domestic-violence-app/app/(tabs)/_layout.tsx
@@ -29,6 +29,12 @@ export default function TabLayout() {
           title: 'Explore',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
+      /><Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="gear" color={color} />,
+        }}
       />
     </Tabs>
   );
