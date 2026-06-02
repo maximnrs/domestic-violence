92601099e72aade113e5c7db5d0fa26385d3df41
maximnrs <maxim.nabers@gmail.com>
2026-06-02 15:44:00 +0200

    Create design-system.js

---
+++ mobile-app-frontend/domestic-violence-app/scripts/design-system.js
@@ -0,0 +1,19 @@
+export const COLORS = {
+  primary: '#1F4D4F',
+  primaryLight: '#2E6F70',
+
+  sage: '#7FA89C',
+  mint: '#A8C5B8',
+
+  background: '#F7F8F7',
+
+  border: '#EAEDED',
+
+  text: '#1E1E1E',
+  textMuted: '#9AA3A3',
+
+  success: '#7FA89C',
+  info: '#5E8BBF',
+  warning: '#E0B663',
+  error: '#C9666B',
+};
